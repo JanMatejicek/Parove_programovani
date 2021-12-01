cc0676868031493ffa59611b94a6acf804a1a7e6
Jan Matějíček <72192628+JanMatejicek@users.noreply.github.com>
2021-12-01 21:50:32 +0100

    Add files via upload

---
+++ Priklad_4/Priklad_4/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace Priklad_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_vypoc = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_vypoc
+            // 
+            this.button_vypoc.Location = new System.Drawing.Point(302, 15);
+            this.button_vypoc.Margin = new System.Windows.Forms.Padding(4);
+            this.button_vypoc.Name = "button_vypoc";
+            this.button_vypoc.Size = new System.Drawing.Size(100, 244);
+            this.button_vypoc.TabIndex = 5;
+            this.button_vypoc.Text = "Výpočet";
+            this.button_vypoc.UseVisualStyleBackColor = true;
+            this.button_vypoc.Click += new System.EventHandler(this.button_vypoc_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 57);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(256, 202);
+            this.panel1.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(45, 139);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 22);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "3";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(45, 76);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 21);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(195, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 15);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(256, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 273);
+            this.Controls.Add(this.button_vypoc);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(433, 320);
+            this.MinimumSize = new System.Drawing.Size(433, 320);
+            this.Name = "Form1";
+            this.Text = "Příklad 4";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_vypoc;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Priklad_4/Priklad_4/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Priklad_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_vypoc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int cislo = int.Parse(comboBox1.SelectedItem.ToString());
+                ulong c1 = ulong.Parse(textBox1.Text);
+                ulong c2 = ulong.Parse(textBox2.Text);
+                ulong c3 = ulong.Parse(textBox3.Text);
+                ulong vys = 1; ulong vys3 = 1; ulong vys2 = 1;
+                string label = comboBox1.SelectedItem.ToString();
+                label1.Text = label;
+                for (int i = 0; i < cislo; i++)
+                {
+                    vys *= c1;
+                    vys2 *= c2;
+                    vys3 *= c3;
+                }
+                /*if(cislo == 1){vys = c1;vys2 = c2;vys3 = c3;}
+                if(cislo == 2){vys2 = c2 * c2; vys = c1 * c1;vys3 = c3 * c3;}
+                if(cislo == 3){vys3 = c3 * c3 * c3;vys = c1 * c1 * c1;vys2 = c2 * c2 * c2;}*/
+                textBox1.Text = vys.ToString();
+                textBox2.Text = vys2.ToString();
+                textBox3.Text = vys3.ToString();
+            }
+            catch { MessageBox.Show("ERROR"); }
+        }
+    }
+}
